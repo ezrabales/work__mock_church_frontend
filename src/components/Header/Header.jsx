092a45cdf0b4302c2109a06305d266c6c1ae4413
ezrabales <bales.ezra@gmail.com>
2026-06-16 16:34:04 -0600

    header animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,30 @@ import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
   const transitionSpanRef = useRef(null);
+  const headerOpenContainerRef = useRef(null);
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [menuIsOpening, setMenuIsOpening] = useState(false);
+  const [menuIsClosing, setMenuIsClosing] = useState(false);
+  const menuOpenTime = 0.8;
 
   function toggleMenuOpen() {
-    setMenuIsOpen((prev) => !prev);
+    if (!menuIsOpen && !menuIsOpening) {
+      setMenuIsOpening(true);
+      setTimeout(() => {
+        setMenuIsOpening(false);
+        setMenuIsOpen(true);
+      }, menuOpenTime * 1000);
+    } else {
+      setMenuIsOpen(false);
+      setMenuIsOpening(false);
+      setMenuIsClosing(true);
+      setTimeout(() => {
+        setMenuIsClosing(false);
+      }, menuOpenTime * 1000);
+    }
   }
 
+  // animate menu button
   useEffect(() => {
     const el = transitionSpanRef.current;
     if (!el) return;
@@ -35,22 +53,52 @@ const Header = () => {
 
     const scale = maxDistance / radius + 7;
 
-    if (menuIsOpen) {
+    if (menuIsOpening) {
+      // opening
       el.style.transform = `scale(${scale})`;
       el.style.opacity = "1";
-    } else {
+    } else if (menuIsOpen) {
+      // menu is open, hide element
+      el.style.opacity = "0";
+    } else if (menuIsClosing) {
+      // closing
       el.style.transform = "scale(1)";
-
-      setTimeout(() => {
-        el.style.opacity = "0";
-      }, 650);
+      el.style.opacity = "1";
+    } else {
+      // menu is closed
+      el.style.opacity = "0";
     }
-  }, [menuIsOpen]);
+  }, [menuIsOpening, menuIsOpen, menuIsClosing]);
+
+  // animate open header
+  useEffect(() => {
+    const el = headerOpenContainerRef.current;
+    if (!el) return;
+
+    if (menuIsOpening) {
+      // opening
+      el.className = "header__open-container header__open-container_opening";
+    } else if (menuIsOpen) {
+      // open
+      el.className = "header__open-container header__open-container_open";
+    } else if (menuIsClosing) {
+      // closing
+      el.className = "header__open-container header__open-container_closing";
+    } else {
+      // closed
+      el.className = "header__open-container";
+    }
+  }, [menuIsOpening, menuIsOpen, menuIsClosing]);
 
   return (
     <>
-      <div className={`header ${menuIsOpen ? "header_open" : ""}`}>
-        <NavLink to={"/"} className="header__logo-container glass">
+      <div
+        className={`header ${menuIsOpen || menuIsOpening ? "header_open" : ""}`}
+      >
+        <NavLink
+          to={"/"}
+          className={`header__logo-container glass ${menuIsOpen || menuIsOpening ? "header__logo-container_open" : ""}`}
+        >
           Church
         </NavLink>
         <span
@@ -66,13 +114,13 @@ const Header = () => {
           onClick={() => toggleMenuOpen()}
         >
           <div
-            className={`header__menu-text-container ${menuIsOpen ? "header__menu-text-container_open" : ""}`}
+            className={`header__menu-text-container ${menuIsOpen || menuIsOpening ? "header__menu-text-container_open" : ""}`}
           >
             <p className="header__menu-text-menu">MENU</p>
             <p className="header__menu-text-close">CLOSE</p>
           </div>
           <div
-            className={`header__menu-btn ${menuIsOpen ? "header__menu-btn_open" : ""}`}
+            className={`header__menu-btn ${menuIsOpen || menuIsOpening ? "header__menu-btn_open" : ""}`}
             aria-label="menu button"
           >
             <span className="header__menu-btn-line" />
@@ -82,7 +130,12 @@ const Header = () => {
         </button>
       </div>
       <div className="header__spacer" />
-      {/* {menuIsOpen && <div className="header_open"></div>} */}
+      <div ref={headerOpenContainerRef} className="header__open-container">
+        <div className="header__open-left">
+          <span className="header__open-divider" />
+        </div>
+        <div className="header__open-right">right</div>
+      </div>
     </>
   );
 };
