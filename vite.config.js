d5fe43ad916ea376261930c245093c8b55f44f18
ezrabales <bales.ezra@gmail.com>
2026-06-19 11:28:12 -0600

    dont minify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,
+  },
   server: {
     port: 3000,
     open: true,
